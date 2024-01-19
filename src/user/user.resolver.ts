fcb2fab97914a9fa1c6649dd22ba871dd69b5cfe
Sagiv <sagivb13@gmail.com>
2024-01-19 10:17:10 +0200

    return id when I create a new user

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,7 @@
 import {
   Args,
   Field,
+  ID,
   Mutation,
   ObjectType,
   Resolver,
@@ -9,6 +10,7 @@ import {
 import { Frequency, Genders } from 'src/schemas/user';
 import { UserService } from './user.service';
 import { Categories } from 'src/schemas/question';
+import { ObjectId } from 'mongoose';
 
 registerEnumType(Frequency, {
   name: 'Frequency',
@@ -24,6 +26,9 @@ registerEnumType(Categories, {
 
 @ObjectType()
 export class UserType {
+  @Field(() => ID!)
+  _id: ObjectId;
+
   @Field()
   firebaseId: string;
 
