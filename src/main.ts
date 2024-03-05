ced8e429e021e91e9c5ae6a1c5021b5d9f88ebdd
Sagiv <sagivb13@gmail.com>
2024-03-05 19:05:37 +0200

    change main to es6

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+const  bootstrap= async ()=> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, '0.0.0.0');
