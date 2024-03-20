702ab4de5c66223211287f7a10e6d6d1b87d08ee
Sagiv <sagivb13@gmail.com>
2024-03-20 12:50:04 +0200

    write more code into comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,51 +1,51 @@
 import {
-   Inject, Logger, 
+  //  Inject, Logger, 
    Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { QuestionModule } from './question/question.module';
+// import { QuestionModule } from './question/question.module';
 import { ConfigModule,
    ConfigService 
   }
     from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UserModule } from './user/user.module';
-import { NotificationModule } from './notification/notification.module';
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+// import { UserModule } from './user/user.module';
+// import { NotificationModule } from './notification/notification.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import * as admin from 'firebase-admin';
-import { UserService } from './user/user.service';
+// import * as admin from 'firebase-admin';
+// import { UserService } from './user/user.service';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      imports: [UserModule],
-      useFactory: async (userService: UserService) => ({
-        autoSchemaFile: true,
-        context: async ({ req, res }) => {
-          let firebaseId: string;
-          if (process?.env?.NODE_ENV?.trim() === 'development') {
-            firebaseId = process.env.USER_UID_FOR_TESTING;
-            Logger.log("Running on test mode", AppModule.name);
-          } else {
-            const token = req?.headers?.authorization?.replace('Bearer ', '');
-            const { uid } = await admin.auth().verifyIdToken(token);
-            firebaseId = uid;
-          }
-          if (req.body.operationName === 'upsertUser') {
-            req['userId'] = firebaseId;
-          } else {
-            const user = await userService.findUserIdByFirebaseId(firebaseId);
-            req['userId'] = user?._id;
-          }
-          return { req, res };
-        },
-      }),
-      inject: [UserService],
-    }),
+    // GraphQLModule.forRootAsync<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   imports: [UserModule],
+    //   useFactory: async (userService: UserService) => ({
+    //     autoSchemaFile: true,
+    //     context: async ({ req, res }) => {
+    //       let firebaseId: string;
+    //       if (process?.env?.NODE_ENV?.trim() === 'development') {
+    //         firebaseId = process.env.USER_UID_FOR_TESTING;
+    //         Logger.log("Running on test mode", AppModule.name);
+    //       } else {
+    //         const token = req?.headers?.authorization?.replace('Bearer ', '');
+    //         const { uid } = await admin.auth().verifyIdToken(token);
+    //         firebaseId = uid;
+    //       }
+    //       if (req.body.operationName === 'upsertUser') {
+    //         req['userId'] = firebaseId;
+    //       } else {
+    //         const user = await userService.findUserIdByFirebaseId(firebaseId);
+    //         req['userId'] = user?._id;
+    //       }
+    //       return { req, res };
+    //     },
+    //   }),
+    //   inject: [UserService],
+    // }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -64,14 +64,14 @@ import { UserService } from './user/user.service';
   providers: [AppService],
 })
 export class AppModule {
-  constructor(@Inject(ConfigService) configService: ConfigService) {
-    const projectId = configService.get<string>('project_id');
-    const privateKey = configService
-      .get<string>('private_key')
-    const clientEmail = configService.get<string>('client_email');
-    admin.initializeApp({
-      credential: admin.credential.cert({ projectId, privateKey, clientEmail }),
-      databaseURL: 'https://xxxxx.firebaseio.com',
-    });
-  }
+  // constructor(@Inject(ConfigService) configService: ConfigService) {
+  //   const projectId = configService.get<string>('project_id');
+  //   const privateKey = configService
+  //     .get<string>('private_key')
+  //   const clientEmail = configService.get<string>('client_email');
+  //   admin.initializeApp({
+  //     credential: admin.credential.cert({ projectId, privateKey, clientEmail }),
+  //     databaseURL: 'https://xxxxx.firebaseio.com',
+  //   });
+  // }
 }
