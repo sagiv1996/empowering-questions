326ed31fce08a167292a10b55dbe1c64bf2bc5f3
Sagiv <sagivb13@gmail.com>
2024-03-20 12:40:46 +0200

    remove few comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,42 +20,42 @@ import { UserService } from './user/user.service';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    // GraphQLModule.forRootAsync<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   imports: [UserModule],
-    //   useFactory: async (userService: UserService) => ({
-    //     autoSchemaFile: true,
-    //     context: async ({ req, res }) => {
-    //       let firebaseId: string;
-    //       if (process?.env?.NODE_ENV?.trim() === 'development') {
-    //         firebaseId = process.env.USER_UID_FOR_TESTING;
-    //         Logger.log("Running on test mode", AppModule.name);
-    //       } else {
-    //         const token = req?.headers?.authorization?.replace('Bearer ', '');
-    //         const { uid } = await admin.auth().verifyIdToken(token);
-    //         firebaseId = uid;
-    //       }
-    //       if (req.body.operationName === 'upsertUser') {
-    //         req['userId'] = firebaseId;
-    //       } else {
-    //         const user = await userService.findUserIdByFirebaseId(firebaseId);
-    //         req['userId'] = user?._id;
-    //       }
-    //       return { req, res };
-    //     },
-    //   }),
-    //   inject: [UserService],
-    // }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [UserModule],
+      useFactory: async (userService: UserService) => ({
+        autoSchemaFile: true,
+        context: async ({ req, res }) => {
+          let firebaseId: string;
+          if (process?.env?.NODE_ENV?.trim() === 'development') {
+            firebaseId = process.env.USER_UID_FOR_TESTING;
+            Logger.log("Running on test mode", AppModule.name);
+          } else {
+            const token = req?.headers?.authorization?.replace('Bearer ', '');
+            const { uid } = await admin.auth().verifyIdToken(token);
+            firebaseId = uid;
+          }
+          if (req.body.operationName === 'upsertUser') {
+            req['userId'] = firebaseId;
+          } else {
+            const user = await userService.findUserIdByFirebaseId(firebaseId);
+            req['userId'] = user?._id;
+          }
+          return { req, res };
+        },
+      }),
+      inject: [UserService],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (config: ConfigService) => ({
-    //     uri: config.get<string>('MONGO_DB_URI'),
-    //   }),
-    //   inject: [ConfigService],
-    // }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     // QuestionModule,
     // UserModule,
     // NotificationModule,
@@ -64,15 +64,14 @@ import { UserService } from './user/user.service';
   providers: [AppService],
 })
 export class AppModule {
-  // constructor(@Inject(ConfigService) configService: ConfigService) {
-  //   const projectId = configService.get<string>('project_id');
-  //   const privateKey = configService
-  //     .get<string>('private_key')
-  //     .replace(/\\n/g, '\n');
-  //   const clientEmail = configService.get<string>('client_email');
-  //   admin.initializeApp({
-  //     credential: admin.credential.cert({ projectId, privateKey, clientEmail }),
-  //     databaseURL: 'https://xxxxx.firebaseio.com',
-  //   });
-  // }
+  constructor(@Inject(ConfigService) configService: ConfigService) {
+    const projectId = configService.get<string>('project_id');
+    const privateKey = configService
+      .get<string>('private_key')
+    const clientEmail = configService.get<string>('client_email');
+    admin.initializeApp({
+      credential: admin.credential.cert({ projectId, privateKey, clientEmail }),
+      databaseURL: 'https://xxxxx.firebaseio.com',
+    });
+  }
 }
