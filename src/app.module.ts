874cc71870ff237fc84d3d71f688bba1e9d7cebb
Sagiv <sagivb13@gmail.com>
2024-05-01 15:21:16 +0300

    formatting code better

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Inject, , Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
